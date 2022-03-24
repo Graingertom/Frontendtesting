1052396625642f2c20e5a4f60b3c8f8c58f08271
Tom Grainger <Tom.Luke.Grainger@gmail.com>
2022-03-24 17:35:55 +0000

    creating empty files

--- tests/layout.test.js
+++ tests/layout.test.js
@@ -1,18 +1,1 @@
-/**
- * @jest-environment jsdom
- */
-
-const fs = require('fs');
-const path = require('path');
-const html = fs.readFileSync(path.resolve(__dirname, '../index.html'), 'utf8');
-
-describe('index.html', () => {
-    beforeEach(() => {
-        document.documentElement.innerHTML = html.toString();
-    })
-
-    test('it has a h1 title', () => {
-        let h1 = document.querySelector('h1');
-        expect(h1.textContent).toContain('Our Testing Practice');
-    })
-})
+// FILL ME IN!
